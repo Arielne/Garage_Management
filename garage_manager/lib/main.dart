51b90506a270b6f1879c7edd1933ad0019694c49
truongnekodua <pt.dangnguyenduytruong@gmail.com>
2026-07-04 12:19:04 +0700

    Khởi tạo kết nối Supabase API tại hàm main

--- garage_manager/lib/main.dart
+++ garage_manager/lib/main.dart
@@ -4,7 +4,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'theme/app_theme.dart';
 import 'core/app_routes.dart';
 
-void main() {
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Supabase.initialize(
+    url: 'https://nywdepmmllfeyfuidtyi.supabase.co',
+    anonKey: 'sb_publishable_fHoiSua2No-R0uhkB6RXUA_VqmX20Em',
+  );
+
   runApp(
     const ProviderScope(
       child: GarageManagerApp(),
