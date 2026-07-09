3f6e514bab0cce93e93b0c71af5142b265d1f00f
truongnekodua <pt.dangnguyenduytruong@gmail.com>
2026-07-09 14:10:44 +0700

    Tính năng (Kho hàng): Kết nối thành công kho và phụ tùng với dữ liệu thật trên Supabase

---
+++ garage_manager/lib/core/supabase_config.dart
@@ -0,0 +1,5 @@
+class SupabaseConfig {
+  static const String url = 'https://nywdepmmllfeyfuidtyi.supabase.co';
+  // Note: Standard Supabase flutter uses anonKey, but if your team uses publishableKey naming:
+  static const String anonKey = 'sb_publishable_fHoiSua2No-R0uhkB6RXUA_VqmX20Em';
+}
