6181be7495b173af3f22c708dc7a2cfa6e0e9456
HG2505 <tranlegiahuy25052005@gmail.com>
2026-07-14 16:25:27 +0700

    cài đặt, khởi tạo các thư viện của supabase, Google Sign-In

--- garage_manager/lib/main.dart
+++ garage_manager/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'theme/app_theme.dart';
 import 'core/app_routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://nywdepmmllfeyfuidtyi.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55d2RlcG1tbGxmZXlmdWlkdHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5OTYxNDEsImV4cCI6MjA5ODU3MjE0MX0.mfD3TWuhjiuDlG5sUXF91mYssi7T9FgLTfUD0uXh5Yc',
+  );
+
   runApp(
     const ProviderScope(
       child: GarageManagerApp(),
