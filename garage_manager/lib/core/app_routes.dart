9ce7f807b43cd7761b9b43981d81ca0e5362f541
Anh 6 Bảnh <virusevil999@gmail.com>
2026-07-15 15:33:24 +0700

    fix lai router

--- garage_manager/lib/core/app_routes.dart
+++ garage_manager/lib/core/app_routes.dart
@@ -11,11 +11,11 @@ import '../features/forms/add_vehicle_form.dart';
 import '../features/manager/customers/customer_detail_screen.dart';
 import '../features/manager/manager_shell.dart';
 import '../features/manager/jobs/appointment_management_screen.dart';
+import '../features/manager/jobs/assign_job_screen.dart';
 import '../features/technician/technician_shell.dart';
 import '../features/technician/job_detail_screen.dart';
 import '../features/technician/notification_screen.dart';
 import '../features/technician/technician_personal_info_screen.dart';
-import '../features/manager/jobs/assign_job_screen.dart';
 import 'fake_data.dart';
 import 'models.dart';
 
@@ -26,23 +26,23 @@ class AppRoutes {
   static const String login = '/login';
   static const String register = '/register';
 
-  // Shells
   static const String customerShell = '/customer/shell';
   static const String managerShell = '/manager/shell';
   static const String technicianShell = '/technician/shell';
 
-  // Sub-pages/Forms
   static const String addCustomer = '/add-customer';
   static const String addVehicle = '/add-vehicle';
   static const String vehicleDetail = '/vehicle-detail';
   static const String invoiceDetail = '/invoice-detail';
   static const String customerDetail = '/manager/customer-detail';
+
   static const String jobDetail = '/technician/job-detail';
   static const String notificationJobs = '/technician/notification';
   static const String technicianPersonalInfo = '/technician/personal-info';
-  static const String assignJob = '/manager/jobs/assign';
   static const String appointmentManagement =
       '/manager/jobs/appointment-management';
+  static const String assignJob = '/manager/jobs/assign';
+
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case splash:
@@ -80,6 +80,12 @@ class AppRoutes {
           builder: (_) => const AppointmentManagementScreen(),
           settings: settings,
         );
+      case assignJob:
+        final apt = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(
+          builder: (_) => AssignJobScreen(appointment: apt),
+          settings: settings,
+        );
       case addCustomer:
         return MaterialPageRoute(
           builder: (_) => const AddCustomerForm(),
@@ -113,7 +119,6 @@ class AppRoutes {
         );
       case jobDetail:
         final jobData = settings.arguments as Map<String, dynamic>;
-
         return MaterialPageRoute(
           builder: (_) => JobDetailScreen(jobData: jobData),
           settings: settings,
@@ -121,12 +126,6 @@ class AppRoutes {
       case notificationJobs:
         return MaterialPageRoute(
           builder: (_) => const NotificationScreen(),
-          settings: settings,
-        );
-      case assignJob:
-        final apt = settings.arguments as Map<String, dynamic>;
-        return MaterialPageRoute(
-          builder: (_) => AssignJobScreen(appointment: apt),
           settings: settings,
         );
       case technicianPersonalInfo:
@@ -137,6 +136,7 @@ class AppRoutes {
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
+            appBar: AppBar(title: const Text('Lỗi điều hướng')),
             body: Center(child: Text('Không tìm thấy trang: ${settings.name}')),
           ),
         );
